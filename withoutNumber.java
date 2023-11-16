0bb3e16de210e9fc56685beb8a7a8b746429f639
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-17 00:04:10 +0530

    Print 1 -100 without using numbers

---
+++ withoutNumber.java
@@ -0,0 +1,10 @@
+public class withoutNumber {
+    public static void main(String[] args) {
+        int start = 1;
+        int end = 100;
+        for(int i = start; i<=end;i++){
+            System.out.println(i);
+        }
+    }
+    
+}
