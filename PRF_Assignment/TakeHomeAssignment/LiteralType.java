25d6b505cf66164c46e97907956cc4dbc412ad49
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-22 18:41:51 +0530

    6 literal Type Completed

--- PRF_Assignment/TakeHomeAssignment/LiteralType.java
+++ PRF_Assignment/TakeHomeAssignment/LiteralType.java
@@ -9,6 +9,27 @@ public class LiteralType {
             System.out.println(decimalLiteral);
             System.out.println(octalLiteral);
             System.out.println(hexadecimalLiteral);
-   
+
+            double standardNotation = 3.14;      // Standard notation
+            double scientificNotation = 5.2e2;   // Scientific notation (520.0 in decimal)
+            
+            System.out.println(standardNotation);
+            System.out.println(scientificNotation);
+
+            char charLiteral = 'A';
+            System.out.println(charLiteral);
+
+            String stringLiteral = "Hello, Java!";
+            System.out.println(stringLiteral);
+
+            boolean trueLiteral = true;
+            boolean falseLiteral = false;
+
+            System.out.println(trueLiteral);
+            System.out.println(falseLiteral);
+
+            String nullValue = null;
+            System.out.println(nullValue);
+
         }
 }
