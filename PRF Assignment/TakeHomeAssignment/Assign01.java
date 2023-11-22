495dca6abd8139763b0869a7cf1b62b5f2b49471
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-22 16:58:05 +0530

    Remove

---
+++ PRF Assignment/TakeHomeAssignment/Assign01.java
@@ -0,0 +1,13 @@
+
+/**
+ * 01Assign
+ */
+public class Assign01 {
+
+    public static void main(String[] args) {
+        System.out.println("Janith Induwara Nanayakkara");
+        System.out.println("Galkatiya");
+        System.out.println("Goviyapana");
+        System.out.println("Ahangama");
+    }
+}
