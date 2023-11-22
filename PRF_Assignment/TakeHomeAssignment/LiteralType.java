3371431646c70c98ccc0e6816923fc5777dddaf2
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-22 18:36:44 +0530

    LiteralType

---
+++ PRF_Assignment/TakeHomeAssignment/LiteralType.java
@@ -0,0 +1,14 @@
+package PRF_Assignment.TakeHomeAssignment;
+
+public class LiteralType {
+        public static void main(String[] args) {
+            int decimalLiteral = 42;         // Decimal
+            int octalLiteral = 052;          // Octal (42 in decimal)
+            int hexadecimalLiteral = 0x2A;    // Hexadecimal (42 in decimal)
+
+            System.out.println(decimalLiteral);
+            System.out.println(octalLiteral);
+            System.out.println(hexadecimalLiteral);
+   
+        }
+}
