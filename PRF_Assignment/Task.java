2c7762991394acd682949b40115e08d6c3183c1e
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-26 19:36:13 +0530

    then taks 08

--- PRF_Assignment/Task.java
+++ PRF_Assignment/Task.java
@@ -6,5 +6,11 @@ public class Task {
         i=100;
         int age = 20;
         System.out.println(age);
+        int k =20;
+        int g =20;
+        int total = k+g;
+        System.out.println(total);
+        int x=10,y = 20;
+        System.out.println("X value is: "+ x +" Y value is: " + y);
     }
 }
