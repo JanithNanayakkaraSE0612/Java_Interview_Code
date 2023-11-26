eb38062178efcf4d93dd160ac9be92e25b4b02f4
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-26 19:23:04 +0530

    Completed

---
+++ PRF_Assignment/Task.java
@@ -0,0 +1,10 @@
+package PRF_Assignment;
+
+public class Task {
+    public static void main(String[] args) {
+        int i ;
+        i=100;
+        int age = 20;
+        System.out.println(age);
+    }
+}
