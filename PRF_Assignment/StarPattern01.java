4822307a341aa757f21fa8f0f3b2fd61704b382b
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-26 18:22:21 +0530

    Complete Second Star Pattern

---
+++ PRF_Assignment/StarPattern01.java
@@ -0,0 +1,13 @@
+package PRF_Assignment;
+
+public class StarPattern01 {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 1; i<=n; i++){
+            for(int j =1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
