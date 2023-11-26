a3d5f119b826c552ee4795a23f3506db0216f6bf
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-27 02:48:23 +0530

    Update

--- PRF_Assignment/TakeHomeAssignment/StarPattern.java
+++ PRF_Assignment/TakeHomeAssignment/StarPattern.java
@@ -2,13 +2,12 @@ package PRF_Assignment.TakeHomeAssignment;
 
 public class StarPattern {
     public static void main(String[] args) {
-        int n = 5;
-
-        for(int i =1; i <=n; i++){
-                for(int j =1; j <=i;j++){
-                    System.out.print("* ");
-                }
-                System.out.println();
+        int count =5;
+        for(int i =1; i<=count;i++){
+            for(int j =1 ; j<i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
         }
     }
 }
