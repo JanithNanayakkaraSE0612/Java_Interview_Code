7cbf81ffcaf9bc29ca4312dd627987b427138592
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-27 02:08:56 +0530

    Complete Diamond Star pattern

---
+++ PRF_Assignment/TakeHomeAssignment/DiamondStarPattern.java
@@ -0,0 +1,24 @@
+package PRF_Assignment.TakeHomeAssignment;
+
+public class DiamondStarPattern {
+    public static void main(String[] args) {
+        for(int i =1; i<4;i++){
+            for(int j=3;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int k =1;k<4-i;k++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}
