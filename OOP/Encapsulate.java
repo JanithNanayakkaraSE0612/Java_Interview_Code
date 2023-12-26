21be51686bd6636a7abc6fbf080c3a9914324370
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-12-26 19:32:23 +0530

    Encap Complete

--- OOP/Encapsulate.java
+++ OOP/Encapsulate.java
@@ -9,7 +9,24 @@ public class Encapsulate {
     private int age;
     private double salary;
 
-    
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getSalary(){
+        return salary;
+    }
+    public void setAge(int newAge){
+        age = newAge;
+    }
+    public void setName(String newName){
+        name = newName;
+    }
+    public void setSalary(double newSalary){
+        salary = newSalary;
+    }
 
 
 }
--- OOP/RunEncap.java
+++ OOP/RunEncap.java
@@ -6,6 +6,8 @@ package OOP;
 public class RunEncap {
 
     public static void main(String[] args) {
-        
+        Encapsulate ens = new Encapsulate();
+        ens.setName("Janith");
+        System.out.println(ens.getName());
     }
 }
