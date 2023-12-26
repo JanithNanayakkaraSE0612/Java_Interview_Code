165f6d686b8e7bba73d9383a9af3ea454df5692f
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-12-26 23:23:21 +0530

    Update

--- OOP/Abstraction.java
+++ OOP/Abstraction.java
@@ -1,5 +1,25 @@
 package OOP;
 
 public abstract class Abstraction {
+    private String name;
+    private int number;
+
+    public Abstraction (String name , int number){
+        System.out.println("Constructor Abstraction");
+        this.name = name;
+        this.number = number;
+    }
+    public void mailCheck(){
+        System.out.println("Mailing Check to"+this.name+" "+this.number);
+    }
+    public String toString(){
+        return name+""+number;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getNumber(){
+        return number;
+    }
     
 }
