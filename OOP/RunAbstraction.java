206ecf86d48a9ea3fb315dc7bcd2395be0a486d4
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-12-26 23:34:54 +0530

    This Way Error

--- OOP/RunAbstraction.java
+++ OOP/RunAbstraction.java
@@ -2,6 +2,8 @@ package OOP;
 
 public class RunAbstraction {
     public static void main(String[] args) {
-        
+        Abstraction abs = new Abstraction("Janith",22);
+        System.out.println("Mail Check");
+        abs.mailCheck();
     }
 }
