2c15ffdd6fab5ad04b22706e32fff261b1ecfe64
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-01-16 04:22:05 +0530

    String Immutable Example

---
+++ StringAPI/StringTest.java
@@ -0,0 +1,9 @@
+package StringAPI;
+
+public class StringTest {
+    public static void main(String[] args) {
+            String name = "Janith";
+            name.concat("Nanayakkara");
+            System.out.println(name);
+    }
+}
