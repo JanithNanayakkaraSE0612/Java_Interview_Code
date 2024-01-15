a64b663c4af8e65edb50406d91e5c58d29f925d1
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-01-16 04:26:12 +0530

    StringBuffer Complete Example code with

---
+++ StringAPI/StringBufferTest.java
@@ -0,0 +1,9 @@
+package StringAPI;
+
+public class StringBufferTest {
+    public static void main(String[] args) {
+        StringBuffer buffer = new StringBuffer("Software");
+        buffer.append("Engineer");
+        System.out.println(buffer);
+    }
+}
