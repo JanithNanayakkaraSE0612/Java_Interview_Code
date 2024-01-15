7fc5e87a4ca77eee9d90b55b13f1f7760ba20642
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-01-16 04:24:30 +0530

    StringBuilder Exaple with code

---
+++ StringAPI/StringBuilderTest.java
@@ -0,0 +1,9 @@
+package StringAPI;
+
+public class StringBuilderTest {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder("Nanayakkara");
+        builder.append("Janith");
+        System.out.println(builder);
+    }
+}
