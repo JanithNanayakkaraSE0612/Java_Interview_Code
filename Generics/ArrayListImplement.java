3f2c4df3205dbb4eb45d015cb37305f34b949ad7
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-01-29 20:43:49 +0530

    Update

---
+++ Generics/ArrayListImplement.java
@@ -0,0 +1,29 @@
+package Generics;
+
+import java.util.ArrayList;
+
+public class ArrayListImplement {
+    
+public static void main(String[] args) {
+     // Declaration and Initialization
+ ArrayList<Integer> arrayList = new ArrayList<>();
+
+
+ // Adding elements
+ arrayList.add(5);
+ arrayList.add(10);
+ arrayList.add(15);
+
+
+ // Accessing elements
+ int element = arrayList.get(1); // Accessing the second element (index 1)
+ System.out.println("element : " + element);
+
+
+ // Iterating through the ArrayList
+ for (int i = 0; i < arrayList.size(); i++) {
+     System.out.println(arrayList.get(i));
+ }
+
+}
+}
---
+++ Generics/HashMapImplement.java
@@ -0,0 +1,30 @@
+package Generics;
+
+import java.util.HashMap;
+
+public class HashMapImplement {
+    public static void main(String[] args) {
+        
+ // Declaration and Initialization
+ HashMap<String, Integer> hashMap = new HashMap<>();
+
+
+ // Adding key-value pairs
+ hashMap.put("One", 1);
+ hashMap.put("Two", 2);
+ hashMap.put("Three", 3);
+
+
+ // Accessing values by key
+ int value = hashMap.get("Two"); // Accessing value associated with key "Two"
+ System.out.println(value);
+
+
+ // Iterating through the HashMap
+ for (String key : hashMap.keySet()) {
+     System.out.println("Key: " + key + ", Value: " + hashMap.get(key));
+ }
+
+
+    }
+}
